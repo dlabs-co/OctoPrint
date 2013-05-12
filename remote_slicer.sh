9d3111f91055769baf3bd9ac032d9b4d6f6513d5
David Francos (XayOn) <opensource@davidfrancos.net>
2013-05-13 00:47:48 +0200

    Moved remote slicer

--- remote_slicer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This script slices in remote a file; Usage: $0 <filename> <remote_ssh>
-filename=$(mktemp) ; cat $1| ssh $2 "cat /dev/stdin > $filename; python /opt/skeinforge/skeinforge_application/skeinforge_utilities/skeinforge_craft.py $filename &>/dev/null; cat ${filename}_export.gcode && rm ${filename}* &>/dev/null "
